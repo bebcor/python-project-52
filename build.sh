b065263c99338e1ac586c38b04993fe84469e57d
иван шулубин <shulubin.ia@gmail.com>
2025-07-05 16:50:35 +0300

    fix builld

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
+set -e  
 
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/activate
 
 
-make install
-make collectstatic
-make migrate
+uv pip install --upgrade pip
+uv pip install -r requirements.txt
+
+
+python manage.py collectstatic --noinput
+python manage.py migrate
