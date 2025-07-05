b5a31c1a747c50654a56e76ffa0e5bccb61acafe
иван шулубин <shulubin.ia@gmail.com>
2025-07-05 17:22:12 +0300

    build23

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/activate
+source $HOME/.local/bin/env  
 
 make install && make collectstatic && make migrate
+
+
+echo "===== PATH DIAGNOSTICS ====="
+echo "Current PATH: $PATH"
+echo "Gunicorn path: $(which gunicorn || echo 'Not found')"
+echo "===== END DIAGNOSTICS ====="
