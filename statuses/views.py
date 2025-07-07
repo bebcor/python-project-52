e795b0ab2eb7c67f5336c86da0b6da10ba0e01e9
иван шулубин <shulubin.ia@gmail.com>
2025-07-07 16:04:38 +0300

    fix task3

--- statuses/views.py
+++ statuses/views.py
@@ -1,44 +1,26 @@
-from django.shortcuts import render
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib import messages
-from django.shortcuts import redirect
 from .models import Status
-from .forms import StatusForm
 
 class StatusesListView(LoginRequiredMixin, ListView):
     model = Status
     template_name = 'statuses/list.html'
     context_object_name = 'statuses'
 
-class StatusCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+class StatusCreateView(LoginRequiredMixin, CreateView):
     model = Status
-    form_class = StatusForm
+    fields = ['name']
     template_name = 'statuses/form.html'
-    success_url = reverse_lazy('statuses_list')
-    success_message = "Статус успешно создан"
+    success_url = reverse_lazy('statuses:list')
 
-class StatusUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+class StatusUpdateView(LoginRequiredMixin, UpdateView):
     model = Status
-    form_class = StatusForm
+    fields = ['name']
     template_name = 'statuses/form.html'
-    success_url = reverse_lazy('statuses_list')
-    success_message = "Статус успешно изменён"
+    success_url = reverse_lazy('statuses:list')
 
 class StatusDeleteView(LoginRequiredMixin, DeleteView):
     model = Status
     template_name = 'statuses/delete.html'
-    success_url = reverse_lazy('statuses_list')
-    success_message = "Статус успешно удалён"
-    
-    def post(self, request, *args, **kwargs):
-        try:
-            return super().post(request, *args, **kwargs)
-        except Exception:
-            messages.error(
-                request,
-                "Невозможно удалить статус, потому что он используется"
-            )
-            return redirect(self.success_url)
+    success_url = reverse_lazy('statuses:list')
