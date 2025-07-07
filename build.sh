e81c8139ffc007fecd16d1fec054019a0ce96210
иван шулубин <shulubin.ia@gmail.com>
2025-07-08 01:27:02 +0300

    mikro fix 2

--- build.sh
+++ build.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
-
-source ~/.venvs/project52/bin/activate
-
-
-uv pip install -r requirements.txt
-
+uv pip install -r requirements.txt --python=3.10
 
 python manage.py migrate
 python manage.py collectstatic --noinput
