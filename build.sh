6b6965aabaa9fa71dca18c07355f0030751fbc77
иван шулубин <shulubin.ia@gmail.com>
2025-07-08 01:31:43 +0300

    fix render0

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
-uv pip install -r requirements.txt --python=3.10
+uv pip install -r requirements.txt --python /opt/render/project/python/Python-3.10.12/bin/python3.10
 
-python manage.py migrate
-python manage.py collectstatic --noinput
+/opt/render/project/python/Python-3.10.12/bin/python3.10 manage.py migrate
+/opt/render/project/python/Python-3.10.12/bin/python3.10 manage.py collectstatic --noinput
