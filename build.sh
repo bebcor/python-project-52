6fa9a8bafa9d697beb3425654a3ede0b5f94045e
иван шулубин <shulubin.ia@gmail.com>
2025-07-08 13:59:07 +0300

    fix

--- build.sh
+++ build.sh
@@ -4,9 +4,11 @@ set -e
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
-
 uv pip install -r requirements.txt --python /opt/render/project/python/Python-3.10.12/bin/python3.10
 
+
+export PYTHONPATH=/opt/render/project/python/Python-3.10.12/bin/python3.10:$PYTHONPATH
+export PYTHONPATH=$(pwd):$PYTHONPATH
 
 /opt/render/project/python/Python-3.10.12/bin/python3.10 manage.py migrate
 /opt/render/project/python/Python-3.10.12/bin/python3.10 manage.py collectstatic --noinput
