3ddd00255b6b7cce5f1554792f6eb96428073bf3
иван шулубин <shulubin.ia@gmail.com>
2025-07-11 22:48:01 +0300

    fix

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -90,7 +90,8 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
         'OPTIONS': {
-            'min_length': 3,  
+            'min_length': 3,}
+    
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
