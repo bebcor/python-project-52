2930b9b9a1e5a4078022e47ac16d18b323d30598
иван шулубин <shulubin.ia@gmail.com>
2025-07-12 20:25:16 +0300

    fix

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -87,7 +87,6 @@ DATABASES = {
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES["default"].update(db_from_env)
 
-FIXTURE_DIRS = [os.path.join(BASE_DIR, 'task_manager/fixtures')]
 
 AUTH_PASSWORD_VALIDATORS = [
     #{
