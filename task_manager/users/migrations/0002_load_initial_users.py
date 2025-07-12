9110a3ba4fd2f2fb74500f8e2ac24b8bc1cad728
иван шулубин <shulubin.ia@gmail.com>
2025-07-12 20:56:54 +0300

    fix

--- task_manager/users/migrations/0002_load_initial_users.py
+++ task_manager/users/migrations/0002_load_initial_users.py
@@ -1,15 +1,37 @@
 from django.db import migrations
+from django.contrib.auth.hashers import make_password
 
-def load_users(apps, schema_editor):
-    from django.core.management import call_command
-    call_command("loaddata", "test_users.json")
+def create_users(apps, schema_editor):
+    User = apps.get_model('users', 'User')
+    users = [
+        {
+            'username': 'john_snow',
+            'password': make_password('Stark123'),
+            'first_name': 'John',
+            'last_name': 'Snow'
+        },
+        {
+            'username': 'daenerys_t',
+            'password': make_password('Dracarys123'),
+            'first_name': 'Daenerys',
+            'last_name': 'Targaryen'
+        },
+        {
+            'username': 'simon_show',
+            'password': make_password('Wine123'),
+            'first_name': 'Simon',
+            'last_name': 'Show'
+        }
+    ]
+    
+    for user_data in users:
+        User.objects.create(**user_data)
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('users', '0001_initial'),
     ]
 
     operations = [
-        migrations.RunPython(load_users),
+        migrations.RunPython(create_users),
     ]
